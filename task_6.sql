a09154b86cc46d22c6c51cb897c28346f7e3c665
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-10-25 14:27:59 +0200

    Populating your database with more data

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,6 @@
 INSERT INTO customers (customer_id, customer_name, email, address)
 VALUES
-    (2, 'Blessing Malik', 'bmalik@sandtech.com','124 Happiness Ave.'),
-    (3, 'Obed Eboneah', 'eobed@sandtech.com','125 Happiness Ave.'),
-    (4, 'Nehemial', 'nkamolu@sandtech.com','126 Happiness Ave.');
+    (2,'Blessing Malik','bmalik@sandtech.com','124 Happiness Ave.'),
+    (3,'Obed Eboneah','eobed@sandtech.com','125 Happiness Ave.'),
+    (4,'Nehemial','nkamolu@sandtech.com','126 Happiness Ave.');
         
