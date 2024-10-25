c39f4cf263298429be29bc55467196121d8de109
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-10-25 03:46:51 +0200

    Full description of the table

--- task_4.sql
+++ task_4.sql
@@ -2,7 +2,7 @@
 SELECT
     TABLE_NAME,
     COLUMN_NAME,
-    DATA_TYPE,
+    COLUMN TYPE,
     CHARACTER_MAXIMUM_LENGTH,
     IS_NULLABLE,
     COLUMN_DEFAULT
@@ -12,4 +12,4 @@ FROM
 
 WHERE
     TABLE_SCHEMA = 'alx_book_store'
-    AND TABLE_NAME = 'books';
+    AND TABLE_NAME = 'Books';
